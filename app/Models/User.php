cc7f261867671e42ef9cca4d022825f084392b85
Duvan0417 <narvaezduvan29@gmail.com>
2025-04-08 16:26:52 -0500

    duvan

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class user extends Model
 {
-    //
+    public function requestAdoption()
+    {
+        return $this->hasOne(RequestAdoption::class, 'user_ID');
+    }
+    
+    public function requestAppointment()
+    {
+        return $this->hasOne(RequestAppointment::class, 'user_ID');
+    }
+    
+    public function shoppingCart()
+    {
+        return $this->hasOne(ShoppingCart::class, 'user_ID');
+    }
+    
+    public function order()
+    {
+        return $this->hasOne(order::class, 'UserID');
+    }
+    
+    public function forum()
+    {
+        return $this->hasOne(Forum::class, 'UserID');
+    }
+    
+    public function paymentmethod ()
+    {
+        return $this->hasOne(paymentmethod::class, 'UserID');
+    }
+    
 }
